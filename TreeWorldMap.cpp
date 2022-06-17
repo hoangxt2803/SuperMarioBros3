bbf18a74a06829d10e6e58d026d910e9a0877c14
hoangxt2803 <hoangxt2803@gmail.com>
2022-06-18 02:04:19 +0700

    update TreeInWorldMap

--- TreeWorldMap.cpp
+++ TreeWorldMap.cpp
@@ -3,7 +3,19 @@
 void CTreeWorldMap::Render()
 {
 	CAnimations* animations = CAnimations::GetInstance();
+	if(type == TYPE_NONE)
+		animations->Get(ID_ANI_TREE_WORLD_MAP)->Render(x, y);
+	else if(type == TYPE_TREE)
+		animations->Get(ID_ANI_NONE_WORLD_MAP)->Render(x, y);
 	animations->Get(ID_ANI_TREE_WORLD_MAP)->Render(x, y);
-
 }
-CTreeWorldMap::CTreeWorldMap(float x, float y) : CGameObject(x, y) {};
+void CTreeWorldMap::GetBoundingBox(float& l, float& t, float& r, float& b)
+{
+	l = x - 12 / 2;
+	t = y - 12 / 2;
+	r = l + 12;
+	b = t + 12;
+}
+CTreeWorldMap::CTreeWorldMap(float x, float y, int type) : CGameObject(x, y) {
+	this->type = type;
+}
--- TreeWorldMap.h
+++ TreeWorldMap.h
@@ -5,13 +5,17 @@
 #include "Animations.h"
 
 #define ID_ANI_TREE_WORLD_MAP 5001
-
+#define ID_ANI_NONE_WORLD_MAP 5005
+#define TYPE_TREE	1
+#define TYPE_NONE	0
 
 class CTreeWorldMap : public CGameObject {
+	virtual int IsCollidable() { return 1; };
+	virtual int IsBlocking() { return 1; }
+	int type;
 public:
-	CTreeWorldMap(float x, float y);
+	CTreeWorldMap(float x, float y, int type);
 	void Render();
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {};
-	void GetBoundingBox(float& l, float& t, float& r, float& b) {};
-	int IsBlocking() { return 1; }
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
 };
